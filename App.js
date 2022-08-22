3b1fdaf146138895a5aa37388c58dc8db93bcfaf
Fumiya Yamanaka <f.yamanaka1127@gmail.com>
2022-08-21 17:16:25 -0700

    Add device-brightness

--- App.js
+++ App.js
@@ -2,28 +2,24 @@ import React, {useState} from 'react';
 import {StyleSheet} from 'react-native';
 import {
   ViroARScene,
-  ViroText,
   ViroARSceneNavigator,
-  ViroTrackingStateConstants,
   Viro3DObject,
   ViroAmbientLight,
   ViroSpotLight,
   ViroARPlaneSelector,
   ViroNode,
 } from '@viro-community/react-viro';
+import DeviceBrightness from '@adrianso/react-native-device-brightness';
 
 const HelloWorldSceneAR = () => {
-  const [text, setText] = useState('Initializing AR...');
   const [isEmojiVisible, setIsEmojiVisible] = useState(true);
+  const [brightness, setBrightness] = useState(0);
 
-  function onInitialized(state, reason) {
+  async function onInitialized(state, reason) {
     console.log('guncelleme', state, reason);
-    if (state === ViroTrackingStateConstants.TRACKING_NORMAL) {
-      setText('Hello World!');
-    } else if (state === ViroTrackingStateConstants.TRACKING_UNAVAILABLE) {
-      // Handle loss of tracking
-      setText('Tracking lost. Please find a surface.');
-    }
+    const brightness = await DeviceBrightness.getBrightnessLevel();
+    console.log(brightness);
+    setBrightness(brightness);
   }
 
   return (
@@ -37,14 +33,6 @@ const HelloWorldSceneAR = () => {
       <ViroSpotLight innerAngle={5} outerAngle={90} />
 
       <ViroARPlaneSelector>
-        {/* <Viro3DObject
-          source={require('./emoji_smile.vrx')}
-          position={[0, 0.1, 0]}
-          scale={[0.2, 0.2, 0.2]}
-          type="VRX"
-          dragType="FixedDistance"
-          onDrag={() => {}}
-        /> */}
         <ViroNode
           position={[0, 0, -1]}
           dragType="FixedToWorld"
